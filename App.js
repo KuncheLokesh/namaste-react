125633634c14415e8ca8de1e6a27a896bd25f164
lokesh k <kunchelokesh57@gmail.com>
2024-10-22 21:10:40 +0530

    lokesh done the commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("div", {id :"parent"}, [ React.createElement("div", {id :"child1"},  React.createElement("h1", {id :"parent"},"child 1 heading" )), React.createElement("div", {id :"child2"}, React.createElement("h1", {id :"child2 heading"},"child 2 heading" ) )]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
