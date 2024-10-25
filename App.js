a515633511b6e17f2f7dcfaeb2de4234383db68b
KuncheLokesh <kunchelokesh57@gmail.com>
2024-10-25 05:57:28 +0530

    day3

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
-const heading = React.createElement("div", {id :"parent1"}, [ React.createElement("div", {id :"child1"},  React.createElement("h1", {id :"parent"},"child 1 heading1" )), React.createElement("div", {id :"child2"}, React.createElement("h1", {id :"child2 heading"},"child 2 heading" ) )]);
+// const heading = React.createElement("div", {id :"parent1"}, [ React.createElement("div", {id :"child1"},  React.createElement("h1", {id :"parent"},"child 1 heading1" )), React.createElement("div", {id :"child2"}, React.createElement("h1", {id :"child2 heading"},"child 2 heading" ) )]);
+const jsxheading=(<h1>namste react from the jsx 🚀
+    <h2>another elemt</h2>
+</h1>);
+
+const Title=()=>{
+    return  <h1>Namaste React Form Functional Component</h1>
+}
+
+const HeadingComponent=()=>{
+   return  (
+    <div>
+        <Title/>
+        <h3>Heading component</h3>
+    </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
